88079e5f0cfab5193897a965aaa5c17bac124006
AkhsanBy <akhsanby@gmail.com>
2022-02-07 12:38:54 +0700

    add nprogress (for styling only)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import NextNprogress from "nextjs-progressbar";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress color="#e70634" startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
