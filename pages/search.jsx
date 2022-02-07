ac4d6cf5d75b426b41a3445bf8015b6deb93a884
AkhsanBy <akhsanby@gmail.com>
2022-02-07 12:41:03 +0700

    search feature is added

--- pages/search.jsx
+++ pages/search.jsx
@@ -5,20 +5,34 @@ import Pagination from "../components/Pagination";
 // styles
 import styles from "../styles/Search.module.scss";
 
-export default function Search() {
+// api
+import { fetchMovieByKeyword } from "../config/api";
+
+export default function Search({ keyword, movieData }) {
   return (
     <Layout title="Search Movie">
       <section id={styles.latest}>
         <div className={styles.latestHeading}>
-          <h1>Result For Movie</h1>
+          <h1>Result For Movie : {keyword}</h1>
         </div>
         <div className={styles.postContainer}>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((el) => (
-            <MovieCard key={el} />
+          {movieData.results.map((movie, index) => (
+            <MovieCard key={index} popularMovie={movie} />
           ))}
         </div>
       </section>
       <Pagination />
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const data = await fetchMovieByKeyword(context.query.keyword);
+
+  return {
+    props: {
+      keyword: context.query.keyword,
+      movieData: data,
+    },
+  };
 }
