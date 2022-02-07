db5b6379f3cc70fbb783e0fd588cab24d299d7d3
AkhsanBy <akhsanby@gmail.com>
2022-02-07 12:40:41 +0700

    detail movie feature is added

--- pages/[detail].jsx
+++ pages/[detail].jsx
@@ -3,12 +3,25 @@ import MovieBanner from "../components/MovieBanner";
 import MovieDescription from "../components/MovieDescription";
 import MovieScreenshots from "../components/MovieScreenshots";
 
-export default function DetailMovie() {
+// api
+import { fetchMoviesById, fetchMovieImagesById } from "../config/api";
+
+export default function DetailMovie({ detailMovie, imagesMovie }) {
   return (
     <Layout title="Detail Movie">
-      <MovieBanner />
-      <MovieDescription />
-      <MovieScreenshots />
+      <MovieBanner movieData={detailMovie} />
+      <MovieDescription title={detailMovie.title} description={detailMovie.overview} />
+      <MovieScreenshots images={imagesMovie} />
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const [detailMovie, imagesMovie] = await Promise.all([fetchMoviesById(context.query.id), fetchMovieImagesById(context.query.id)]);
+  return {
+    props: {
+      detailMovie,
+      imagesMovie,
+    },
+  };
 }
