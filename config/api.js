c2d3e1822af38ab7c5130b4420336bb499752353
AkhsanBy <akhsanby@gmail.com>
2022-02-07 12:37:33 +0700

    add new function to fetch "image, by id, by keyword"

--- config/api.js
+++ config/api.js
@@ -1,18 +1,31 @@
 const baseUrl = "https://api.themoviedb.org/3";
-const firstParameter = ["movie"];
-const secondParameter = ["popular", "upcoming"];
+const firstParameter = ["movie", "search"];
+const secondParameter = ["popular", "upcoming", "movie"];
 const apiKey = "ee4cefe9990dc2b171852cb4023bab21";
 const language = ["en-US"];
 const page = ["1"];
 
 export async function fetchPopularMovies() {
   const response = await fetch(`${baseUrl}/${firstParameter[0]}/${secondParameter[0]}?api_key=${apiKey}`);
-  const data = await response.json();
-  return data;
+  return await response.json();
 }
 
 export async function fetchUpcomingMovies() {
   const response = await fetch(`${baseUrl}/${firstParameter[0]}/${secondParameter[1]}?api_key=${apiKey}&language=${language[0]}`);
-  const data = await response.json();
-  return data;
+  return await response.json();
+}
+
+export async function fetchMoviesById(movieId) {
+  const response = await fetch(`${baseUrl}/${firstParameter[0]}/${movieId}?api_key=${apiKey}`);
+  return await response.json();
+}
+
+export async function fetchMovieImagesById(movieId) {
+  const response = await fetch(`${baseUrl}/${firstParameter[0]}/${movieId}/images?api_key=${apiKey}`);
+  return await response.json();
+}
+
+export async function fetchMovieByKeyword(keyword) {
+  const response = await fetch(`${baseUrl}/${firstParameter[1]}/${secondParameter[2]}?api_key=${apiKey}&query=${keyword}`);
+  return await response.json();
 }
