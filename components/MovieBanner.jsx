31c96639cf74c5d83bd4b9ccabdf81feb7912476
AkhsanBy <akhsanby@gmail.com>
2022-02-08 12:47:27 +0700

    change tag img to tag Image

--- components/MovieBanner.jsx
+++ components/MovieBanner.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-no-target-blank */
 import { Fragment } from "react";
+import Image from "next/image";
 
 // styles
 import styles from "../styles/MovieBanner.module.scss";
@@ -11,10 +12,12 @@ import { imageBaseUrl } from "../config/image";
 import { displayGenre, displayDate } from "../config/other";
 
 export default function MovieBanner({ movieData }) {
+  const videoTrailer = movieData.videos.results.find((video) => video.type === "Trailer" && video.official === true);
+
   return (
     <section className={styles.movieBanner}>
       <div className={styles.movieBannerImg}>
-        <img src={imageBaseUrl(movieData.backdrop_path)} alt="banner" />
+        <Image src={imageBaseUrl(movieData.backdrop_path)} alt="banner" layout="fill" />
       </div>
 
       <div className={styles.bannerContainer}>
@@ -43,30 +46,17 @@ export default function MovieBanner({ movieData }) {
               <br />
               {movieData.genres.map((genre, index) => (
                 <Fragment key={index}>
-                  <a href="">{displayGenre(genre.id)}</a>
+                  <span>{displayGenre(genre.id)}</span>
                 </Fragment>
               ))}
             </div>
-            <a href="https://www.youtube.com/watch?v=ZgQkEf3dQ08" target="_blank" className={styles.watchBtn}>
-              Watch Trailer
-            </a>
-          </div>
-        </div>
-        <div className={styles.playBtnContainer}>
-          <div className={styles.playBtn}>
-            <a href="javascript:void">
-              <i className="fas fa-play"></i>
-            </a>
-          </div>
-        </div>
-        <div id="play" className={styles.play}>
-          <a href="javascript:void" className={styles.closeMovie}>
-            <i className="fas fa-times"></i>
-          </a>
-          <div className={styles.playMovie}>
-            <video id="m-video" controls>
-              <source src="/Movie-Data/Pirates-of-the-Caribbean-Salazar's-Revenge/Movie.mp4" type="video/mp4" />
-            </video>
+            {videoTrailer ? (
+              <a href={`https://www.youtube.com/watch?v=${videoTrailer.key}`} target="_blank" className={styles.watchBtn}>
+                Watch Trailer
+              </a>
+            ) : (
+              <a className={styles.watchBtnDisabled}>Trailer Not Available</a>
+            )}
           </div>
         </div>
       </div>
