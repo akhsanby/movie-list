a4bb1a379be13cd1252c229bbb5df914d9eb47fc
AkhsanBy <akhsanby@gmail.com>
2022-02-08 12:51:29 +0700

    add new function imageBaseUrl

--- config/other.js
+++ config/other.js
@@ -101,3 +101,7 @@ export function convertToSlug(text) {
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "");
 }
+
+export function imageBaseUrl(poster_path) {
+  return `https://image.tmdb.org/t/p/w500${poster_path}`;
+}
